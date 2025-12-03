529da08a9ce1e52908476b59ead5898d77c28fbc
GerhartPapaGee <55330223+GerhartPapaGee@users.noreply.github.com>
2025-12-02 23:29:13 -0500

    Initial commit - AboveTheLaw corrupt cop mod for LSR

---
+++ AboveTheLaw/AboveTheLaw/SettingsManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+public class SettingsManager
+{
+    private string SettingsFile;
+
+    public Keys MenuKey { get; set; } = Keys.F3;
+    public Keys PullOverKey { get; set; } = Keys.E;
+    public Keys CuffKey { get; set; } = Keys.T;
+    public Keys BackseatKey { get; set; } = Keys.Y;
+
+    public SettingsManager(string folder)
+    {
+        SettingsFile = Path.Combine(folder, "AboveTheLaw_Settings.xml");
+    }
+
+    public void LoadSettings()
+    {
+        if (!File.Exists(SettingsFile))
+        {
+            SaveSettings();
+            return;
+        }
+
+        try
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SettingsManager));
+            using (FileStream fs = new FileStream(SettingsFile, FileMode.Open))
+            {
+                SettingsManager loaded = (SettingsManager)serializer.Deserialize(fs);
+                MenuKey = loaded.MenuKey;
+                PullOverKey = loaded.PullOverKey;
+                CuffKey = loaded.CuffKey;
+                BackseatKey = loaded.BackseatKey;
+            }
+        }
+        catch { SaveSettings(); }
+    }
+
+    public void SaveSettings()
+    {
+        try
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SettingsManager));
+            using (FileStream fs = new FileStream(SettingsFile, FileMode.Create))
+            {
+                serializer.Serialize(fs, this);
+            }
+        }
+        catch { }
+    }
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+# AboveTheLaw – Los Santos Red Corrupt Cop Mod
+
+A RAGE Plugin Hook mod for GTA V with Los Santos RED integration.
+
+## Features
+- Pull over vehicles with E (in car)
+- Order driver out with E (on foot)
+- Cuff suspects with T (real police animation)
+- Secure in backseat with Y (they stay forever)
+- Book suspects at jail locations with H (5 stations including BCSO Sandy Shores)
+
+## Installation
+1. Install RAGE Plugin Hook, Script Hook V, Script Hook V .NET (nightly), and Los Santos RED.
+2. Copy `AboveTheLaw.dll` to `GTA V/plugins/AboveTheLaw/`.
+3. Copy `AboveTheLaw_Settings.xml` to `GTA V/plugins/AboveTheLaw/XML/`.
+4. Copy All xmls in the `AboveTheLawXMLConfiguration` to `plugins/LosSantosRED/` folder.
+
+## Default Keys
+- E = Pull Over / Order Out
+- T = Cuff / Uncuff
+- Y = Backseat In/Out
+- F3 = Menu (Let Go)
+- H = Jail Drop-off
+
+Edit `AboveTheLaw_Settings.xml` to change keys.
+
+## Booking Locations
+- BCSO Sandy Shores
+- Paleto Bay SO
+- La Mesa PD
+- Mission Row PD
+- Los Santos SO
+
+## Mp Maps need to be enabled in Los Santos Red
+
+Look for the blue cylinder marker at each station.
+
+## Credits
+- Built with RAGE Plugin Hook
+- Requires Los Santos RED by Greskrendtregk
+- Original concept by GerhartPapaGee, Gerhart, GerhartRTFO, PapaGee
+
+## License
+MIT – do whatever you want just please give credit.
